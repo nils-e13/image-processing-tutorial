f1650c0c7e6d031b834ba449cfadf4600060ff8f
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2022-01-19 21:01:08 +0100

    Add data and improve the main script.

--- image-classification-hugging-face/main.py
+++ image-classification-hugging-face/main.py
@@ -1,51 +1,4 @@
-# This is the python script for the image processing tutorial.
-
-
-"""
-# Complete the following tasks for your individual assignment.
-
-Suppose you work with the city of Amsterdam on automating garbage collection.
-Your first application is to build a trash bin recognition tool.
-The tool will be deployed on the Scan Car system with cameras.
-For more background information, please refer to the following:
-- https://www.ams-institute.org/documents/33/200122_ScanCarBooklet.pdf
-
-For this tutorial, we are going to use a machine learning model on Hugging Face.
-Hugging Face is a nice service that has a collection of pre-trained machine learning models.
-A "pre-trained" model means someone already trained the model using other data.
-Modern models can be hard to train (in terms of the amount of needed data and time).
-The benefit of using a pre-trained model is that someone already did the hard work for you.
-So, you save time collecting a large dataset to train and tune the model.
-You can check https://huggingface.co/models for a list of models.
-In this case, we are using a pre-trained Vision Transformer model.
-You can find details on the following page:
-- https://huggingface.co/google/vit-base-patch16-224
-
-Task 1: Play with the model by feeding different trash bin images to it.
-- You need to collect trash bin images by yourself (e.g., take photos outside, online image search).
-- In the written report, you need to explain how you get the images.
-- Use the web interface on https://huggingface.co/google/vit-base-patch16-224 for this task.
-
-Deliverable 1.1: Give 5 examples that the model works well.
-- The 5 examples need to be different in some way.
-- For instance, they can differ in the type or color of trash bins.
-- You need to put both the images and the predicted result from the model in the written report.
-
-Deliverable 1.2: Give 5 examples that the model does not work well, and explain why they may not work.
-- Hint: check the model details and understand the data that are used to train the model.
-- The 5 examples also need to be different in some way.
-- For instance, think about trash bin types, orientations of the bin, occlusions, weather, etc.
-- You need to put both the images and the predicted result from the model in the written report.
-- You also need to explain why these examples are not working well in the report.
-"""
-
-
-"""
-# Automate the trash bin recognition tool.
-
-Now, you have a understanding about how the model works.
-For the next task, we are going to automate the trash bin recognition tool.
-"""
+# This is the python script for the image processing module.
 
 
 """
@@ -66,14 +19,16 @@ import requests
 In this tutorial, we are going to use the Hugging Face API.
 API means the Application Programming Interface, which allows computer programs to talk to each other.
 API_URL is the Hugging Face API URL that points to a model that we want to use.
+For more information about the model, see the following page:
+- https://huggingface.co/google/vit-base-patch16-224
+
 API_KEY is the Hugging Face API key for authentication (only used for this course).
 Please do not make the API key public.
 For more information about how to use the API, see the following page:
 - https://api-inference.huggingface.co/docs/python/html/quicktour.html
 """
 API_URL = "https://api-inference.huggingface.co/models/google/vit-base-patch16-224"
-#API_KEY = "[PLACE_HOLDER]"
-API_KEY = "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"
+API_KEY = "[PLACE_HOLDER]"
 
 
 # Below is a reusable function for interacting with the Hugging Face API.
@@ -131,14 +86,5 @@ It means that the model thinks there are Egyptian cats in the image, with 0.937 
 print(data)
 
 
-"""
-- You can make use of the "query" function in this script.
-- To allow the "query" function to access the images, you may need to upload them to the tool.
-"""
-# Write your code below for this task.
-
-
-"""
-Pause before the next part of the tutorial.
-"""
+# Pause before the next part of the tutorial.
 input("Press Enter to continue...")
